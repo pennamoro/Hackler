3aae75e39de3bedcbcf9b27b55681d7a40a6ed45
pennamoro <gpennamoro@gmail.com>
2019-10-14 17:13:09 -0300

    Splash

---
+++ app/src/main/java/ifpr/br/tcc/Login/Splash.java
@@ -0,0 +1,27 @@
+package ifpr.br.tcc.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import ifpr.br.tcc.R;
+
+public class Splash extends AppCompatActivity {
+private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
